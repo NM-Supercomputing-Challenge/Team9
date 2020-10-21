1f72268b9bd2160a9f1a584b7e1946006cf9a85c
Joshua  Tamarra <s89291@sfps.k12.nm.us>
2020-10-21 19:21:24 +0000

    First 7 lines

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print ("test")
+import math
+import matplotlib.pyplot as plt
+avgDailyUsage = float(input("Your average usage per day from your Utility bill: "))
+avgDailyCost = float(input("Your average electricity cost per day: "))
+print()
+print("Your annual electricity usage is about {:,.0f} kWh.".format(avgDailyUsage*365))   #curly brackets indicate the placeholder for variable, comma is used to indicate the thousands place and .0 means that there will be no decimal values, and f means that this variable uses float as a data type 
+print("Your annual electricity cost is about ${:,.0f}.".format(avgDailyCost*365))
