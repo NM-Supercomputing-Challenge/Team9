cbb28d2d209cb5e913bd62202e52e59af2fee5c0
Christopher Mendoza <mendozachristopher0828@gmail.com>
2020-11-04 20:22:11 +0000

    Defined ideal function

--- idealModel.py
+++ idealModel.py
@@ -17,3 +17,17 @@ import matplotlib.pyplot as plt
 # ? = deg * pi / 180
 def Deg2Rad(x): return (x*math.pi/180)
  
+# IdealModel is the 'best case' result from PV.
+# The formula used in this function refers to a shadow
+# produced by the PV panel - the larger the shadow the more
+# light falls on it; and it's calculated by multiplying a sine of an
+# angle, at which the sunlight falls on the panel, by 100%.
+# The sine function is the ratio of the side opposite a given
+# angle (in a right triangle) to the hypotenuse.
+# Assuming that we will use 1 as the length of the hypotenuse,
+# which represents the PV panel, and that the side opposite the angle
+# represents the percentage of the shadow, the formula of the sine will be:
+# sin(angle in radians) = shadow / length of PV panel; and:
+# shadow in % = length of PV panel (= 1) * sin(angle in radians) * 100:
+def IdealModel(x): return round(math.sin(Deg2Rad(x))*100, 1)
+# round(num1, num2) rounds num1 to num2 precision
