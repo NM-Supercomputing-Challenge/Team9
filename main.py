335986b96e9055504d5b51a179fff7f9a481c661
Joshua  Tamarra <s89291@sfps.k12.nm.us>
2020-11-04 19:42:37 +0000

    Created new python file

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ print()
 print("Your annual electricity usage is about {:,.0f} kWh.".format(avgDailyUsage*365))   #curly brackets indicate the placeholder for variable, comma is used to indicate the thousands place and .0 means that there will be no decimal values, and f means that this variable uses float as a data type 
 print("Your annual electricity cost is about ${:,.0f}.".format(avgDailyCost*365))
 
-def pricePayoff(size): 
+def pricePayoff(size, prod):
   print()
   panels = math.ceil(size/275) #275 watts is produced by a standard polycrystaline solar panel 
   print("Approximate NUMBER of PANELS: "+str(panels)+';')
